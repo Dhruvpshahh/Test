168da059180efbc1f6c7a7caa5da625886d332dd
Dhruvpshahh <73695581+Dhruvpshahh@users.noreply.github.com>
2022-06-26 01:38:51 +0530

    Updated Auction Page

--- lib/pages/AuctionPage.dart
+++ lib/pages/AuctionPage.dart
@@ -1,8 +1,11 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
+import 'package:intl/intl.dart';
 import 'package:testx/model/new_item.dart';
 
 class MainAuctionPage extends StatefulWidget {
-  const MainAuctionPage({Key? key, required AuctionPageModel auctionModel}) : super(key: key);
+  AuctionPageModel auctionModel;
+  MainAuctionPage({Key? key, required this.auctionModel}) : super(key: key);
 
   @override
   State<MainAuctionPage> createState() => _MainAuctionPageState();
@@ -20,63 +23,122 @@ class _MainAuctionPageState extends State<MainAuctionPage> {
         centerTitle: true,
         title: Text('Auction Page'),
       ),
-      body: Container(
-      width: _width,
-    height: _height,
-    child: Column(
-      children: <Widget>[
-        Row(
-          children: <Widget>[
-            Expanded(
-              child:new Padding(padding: const EdgeInsets.all(20.0),
-                child:Center(
-                  child: Text(
-                    'Title',
+      body: StreamBuilder(
+        stream: FirebaseFirestore.instance
+            .collection("auction_items")
+            .doc(widget.auctionModel.id)
+            .snapshots(),
+        builder: (context, AsyncSnapshot<DocumentSnapshot> snapshot) {
+          if (snapshot.hasData) {
+            widget.auctionModel.bidPrice = snapshot.data!["bidPrice"];
+
+            return Container(
+              width: _width,
+              height: _height,
+              child: Column(
+                children: <Widget>[
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: new Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Center(
+                          child: Text(
+                            widget.auctionModel.title,
+                            style: TextStyle(fontSize: 30),
+                          ),
+                        ),
+                      )),
+                    ],
                   ),
-                ),
-              )
-            ),
-        ],
-        ),
-        SizedBox(height: 20),
-        Row(
-          children: <Widget>[
-            Expanded(
-                child:new Padding(padding: const EdgeInsets.all(20.0),
-                  child:Center(
-                    child: Text(
-                      'Seller Email: xyz@gmail.com',
-                    ),
+                  SizedBox(height: 5),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: new Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Center(
+                          child: Text(
+                            "Seller Email: " + widget.auctionModel.sellerEmail,
+                            style: TextStyle(fontSize: 16),
+                          ),
+                        ),
+                      )),
+                    ],
                   ),
-                )
-            ),
-          ],
-        ),
-        SizedBox(height: 20),
-        Row(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Center(
-              child: ConstrainedBox(
-                constraints: BoxConstraints(maxHeight: 105, maxWidth: 135),
-                child: ClipRRect(
-                  borderRadius: BorderRadius.only(
-                      topRight: Radius.circular(21),
-                      bottomRight: Radius.circular(21)),
-                  child: Container(
-                    alignment: Alignment.center,
-                    width: screenWidth * 0.35,
-                    decoration: BoxDecoration(
-                        image: DecorationImage(
-                            image: NetworkImage('https://images.unsplash.com/photo-1542840843-3349799cded6?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Nnx8c21hbGwlMjBzaXplfGVufDB8fDB8fA%3D%3D&w=1000&q=80'), fit: BoxFit.cover)),
+                  SizedBox(height: 20),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      Center(
+                        child: ConstrainedBox(
+                          constraints: BoxConstraints(maxHeight: 205),
+                          child: ClipRRect(
+                            borderRadius: BorderRadius.only(
+                              topRight: Radius.circular(5),
+                              bottomRight: Radius.circular(5),
+                              topLeft: Radius.circular(5),
+                              bottomLeft: Radius.circular(5),
+                            ),
+                            child: Container(
+                              alignment: Alignment.center,
+                              width: screenWidth * 0.90,
+                              decoration: BoxDecoration(
+                                  image: DecorationImage(
+                                      image: NetworkImage(
+                                          widget.auctionModel.imageURL),
+                                      fit: BoxFit.fill)),
+                            ),
+                          ),
+                        ),
+                      )
+                    ],
                   ),
-                ),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Center(
+                          child: Text(
+                            "Start Time: ${DateFormat.yMMMd().add_jm().format(snapshot.data!["timestamp"].toDate().toLocal())}",
+                            style: TextStyle(fontSize: 18),
+                          ),
+                        ),
+                      )),
+                    ],
+                  ),
+                  const SizedBox(height: 5),
+                  Row(
+                    children: <Widget>[
+                      Expanded(
+                          child: Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Center(
+                            child: Visibility(
+                          visible: widget.auctionModel.date
+                                      .isBefore(DateTime.now()) &&
+                                  widget.auctionModel.date
+                                      .add(Duration(minutes: 30))
+                                      .isAfter(DateTime.now())
+                              ? true
+                              : false,
+                          child: Text((snapshot.data!["bidder"] == ""
+                              ? "No Bids yet"
+                              : "bidder email : " + snapshot.data!["bidder"])),
+                        )),
+                      )),
+                    ],
+                  ),
+                  SizedBox(height: 20),
+                ],
               ),
-            )
-          ],
-        ),
-      ],
-    ),
+            );
+          }
+          return Center(
+            child: CircularProgressIndicator(),
+          );
+        },
       ),
     );
   }
